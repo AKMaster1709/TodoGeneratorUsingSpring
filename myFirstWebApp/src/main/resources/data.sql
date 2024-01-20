be355a140d665b314c30b445ba57545d0c755099
Abhishek Jain <101470572+AKMaster1709@users.noreply.github.com>
2024-01-20 22:38:59 +0530

    Add files via upload

---
+++ myFirstWebApp/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10001,'Abhishek', 'Get AWS Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10002,'Abhishek', 'Learn Google Cloud', CURRENT_DATE(), false);
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10003,'AKmaster', 'Machine Learning', CURRENT_DATE(), false);
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10004,'AKmaster', 'Web Development', CURRENT_DATE(), false);
